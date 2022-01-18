a5cfbfbbb62b7d01a5ea415e4baf535f49fbd656
Hrutvik Yadav <hrutvikyadav@gmail.com>
2022-01-18 22:44:49 +0530

    completed exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(str) {
-
+    let prevIndex = str.length-1;
+    let newIndex = 0,
+        reversedString = '',
+        extractedElement;
+    for(let i = 0; i <= str.length-1; i++) {
+        extractedElement = str.charAt(prevIndex);
+        reversedString += `${extractedElement}`;
+        prevIndex--;
+        newIndex++;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return 'ERROR';
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    let total = 0;
+    if(start<end) {
+        for(let i = start; i <=end; i++) {
+            total += i;
+          }
+    } else {
+        for(let i = start; i >=end; i--) {
+            total += i;
+          }
+    }
+    return total;
 };
 
 // Do not edit below this line
